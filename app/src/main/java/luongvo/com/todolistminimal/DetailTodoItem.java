0af528d2804c1ea206aa0f8f00a9cea8cec86ba1
Luong Vo <vo.tran.thanh.luong@gmail.com>
2017-07-24 00:23:32 +0700

    Add basic layout for todoitem detail

---
+++ app/src/main/java/luongvo/com/todolistminimal/DetailTodoItem.java
@@ -0,0 +1,13 @@
+package luongvo.com.todolistminimal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailTodoItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_todo_item);
+    }
+}
